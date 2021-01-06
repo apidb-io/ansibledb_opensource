99f8af9c636941a408123569753e80da1d342c3c
Ubuntu <ubuntu@ip-172-31-33-215.eu-west-1.compute.internal>
2021-01-06 16:48:24 +0000

    minor updates

--- plugins/modules/apidb.py
+++ plugins/modules/apidb.py
@@ -7,46 +7,31 @@ import json
 import requests
 import subprocess
 
-def getkeys(p):
-    API_KEY =  p["apidbtoken"]
-#    API_ENDPOINT = "https://" + p["apidbendpoint"] + ".apidb.io/api/ansiblerestrictedkeys"
-    API_ENDPOINT = "http://127.0.0.1:5000/"
-    headers = {
-    'Authorization': "Token " + API_KEY,
-    'Content-Type': 'text/json',
-    'Accept':'application/json'
-    }
-    r = requests.get(url = API_ENDPOINT, headers=headers)
-    result = r.json()
-    with open('collections/ansible_collections/apidb/apidb_collection/roles/apidb_post/library/keys_to_sanitise.json', 'wb') as file:
-        file.write(result)
 
+def sanitiseDict(d):
 
-def processdata(p):
-    API_KEY =  p["apidbtoken"]
-#    API_ENDPOINT = "https://" + p["apidbendpoint"] + ".apidb.io/api/processserverdata"
-    API_ENDPOINT = "http://127.0.0.1:5000"
-    headers = {
-    'Authorization': "Token " + API_KEY,
-    'Content-Type': 'text/json',
-    'Accept':'application/json'
-    }
-    r = requests.get(url = API_ENDPOINT, headers=headers)
-    result = r.json()
+    sankeys = open("collections/ansible_collections/apidb/apidb_collection/roles/apidb_post/library/keys_to_sanitise.json", 'r').read()
+    keys = json.loads(sankeys)
+    keys_to_sanitise = keys['keys_to_sanitise']
+    for k in keys_to_sanitise:
+        if k in d.keys():
+            d.update({k: 'EXCLUDED'})
+    for v in d.values():
+        if isinstance(v, dict):
+            sanitiseDict(v)
+    return d
 
 
 def apidb(directory,p,apiendpoint,filename):
-    API_KEY =  p["apidbtoken"]
-#    API_ENDPOINT = "https://" + p["apidbendpoint"] + ".apidb.io/api/" + apiendpoint
-    API_ENDPOINT = "http://127.0.0.1:5000"
+    API_ENDPOINT = p["ansibledb_server"] + "/api/" + apiendpoint ## this will be http://35.177.212.236:5000 we should actually include http in the param too
+    #API_ENDPOINT = "http://localhost:5000/api/" + apiendpoint
     headers = {
-        'Authorization': "Token " + API_KEY,
         'Content-Type': 'text/json',
         'Accept':'application/json'
      }
     data = open(directory + filename, 'rb').read()
-    jdata = sanitiseDict(json.loads)
-    r = requests.post(url = API_ENDPOINT, headers=headers, data=json.jdata))
+    jdata = sanitiseDict(json.loads(data))
+    r = requests.post(url = API_ENDPOINT, headers=headers, data=json.dumps(jdata))
     result = r.text
     statuscode = r.status_code
     meta = {"statuscode" : statuscode, "keys_to_sanitise" : 'key', "endpoint": API_ENDPOINT}
@@ -70,59 +55,20 @@ def postdata(p):
     return is_error, has_changed, meta
 
 
-def kubernetes(p):
-    statuscode = ''
-    result = ''
-    is_error = False
-    has_changed = False
-    meta = {"statuscode" : statuscode, "response" : result}
-    directory='/tmp/kubernetes_files'
-    for root, dirs, files in os.walk(directory):
-        for dir in dirs:
-            nodesfile = ".tmp.cluster-state.nodes.json"
-            for clusterroot, clusterdirs, clusterfiles in os.walk(directory + "/" + dir):
-                for c in clusterfiles:
-                    if c ==  ".tmp.cluster-state.nodes.json":
-                        endpoint = "plugins/kubernetes/" + dir + "/nodes"
-                        fullpath=directory + "/" + dir + "/"
-                        apidb(fullpath,p,endpoint,c)
-                    elif c ==  ".cluster-info.json":
-                        endpoint="plugins/kubernetes/" + dir + "/cluster"
-                        fullpath=directory + "/" + dir + "/"
-                        apidb(fullpath,p,endpoint,c)
-                    elif  "pods.json" in c:
-                        endpoint="plugins/kubernetes/" + dir + "/pods"
-                        fullpath=directory + "/" + dir + "/"
-                        apidb(fullpath,p,endpoint,c)
-                    elif  "deployments.json" in c:
-                        endpoint="plugins/kubernetes/" + dir + "/deployments"
-                        fullpath=directory + "/" + dir + "/"
-                        apidb(fullpath,p,endpoint,c)
-    return is_error, has_changed, meta
-
-
-
 def main():
-
     fields = {
-        "apidbtoken": {"default": "apikey", "type": "str"},
-        "apidbendpoint": {"default": "app", "type": "str"},
+        "ansibledb_server": {"default": "test", "type": "str"},
         "threads": {"default": 3, "type": "int"},
         "chunksize": {"default": 10, "type": "int"},
     }
 
     module = AnsibleModule(argument_spec=fields)
-    getkeys(module.params)
     is_error, has_changed, result = postdata(module.params)
-    is_error, has_changed, result = kubernetes(module.params)
-    processdata(module.params)
 
     if not is_error:
         module.exit_json(changed=has_changed, meta=result)
     else:
         module.fail_json(msg="Error", meta=result)
-
-
 
 if __name__ == '__main__':
     main()
